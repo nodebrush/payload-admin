f2b4b86d0a2bc7e6c145e4b687d9d1d651360444
Samuel Hasselblom <samuel@nodebrush.com>
2026-04-21 13:21:59 +0200

    fix proper admin redirect

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      { source: '/', destination: '/admin', permanent: true },
+    ]
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
